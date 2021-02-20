cd1d39ea44e7e9d5d7feca8087d09a4a6cd4b5f2
Stefan <stefischer@mail.com>
2021-02-20 04:22:23 -0500

    Added update method to mentorsDB

--- API/models/mentorsDB.js
+++ API/models/mentorsDB.js
@@ -10,13 +10,18 @@ module.exports = {
   findByUserId(data) {
     return db.one("SELECT * FROM mentors WHERE users_id = $1", data);
   },
-  save(mentors) {
-    return db.one(
-      `INSERT INTO mentors (id, created_at) 
-      VALUES ($/id/, $/created_at/)
-      RETURNING *`,
-      mentors
-    );
+  save(mentor) {
+    return db.one(`INSERT INTO mentors (users_id, created_at) 
+      VALUES ($/users_id/, $/created_at/)
+      RETURNING *`, mentor);
+  },
+  update(mentor) {
+    return db.one(`UPDATE mentors
+    SET
+    active = $/active/,
+    bio = $/bio/
+    WHERE id = $/id/ AND users_id = $/users_id/
+    RETURNING *`, mentor);
   },
   delete(id) {
     return db.none("DELETE FROM mentors WHERE id = $1", id);

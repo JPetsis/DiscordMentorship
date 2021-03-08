a7a59d84377ebbfe91453119f4a2405ee8688c2e
Stefan <Stefischer@mail.com>
2021-03-08 17:48:28 -0500

    Updated discordTokensDB table name from discord_tokens to discord_login_tokens

--- API/models/discordTokensDB.js
+++ API/models/discordTokensDB.js
@@ -2,21 +2,21 @@ const db = require('../config/connection');
 
 module.exports = {
     findOne(id) {
-        return db.one('SELECT * FROM discord_tokens WHERE id = $1', id);
+        return db.one('SELECT * FROM discord_login_tokens WHERE id = $1', id);
     },
     findByDiscordId(id) {
-        return db.one('SELECT * FROM discord_tokens WHERE discord_id = $1', id);
+        return db.one('SELECT * FROM discord_login_tokens WHERE discord_id = $1', id);
     },
     findByToken(token) {
-        return db.one('SELECT * FROM discord_tokens WHERE access_token = $1', token);
+        return db.one('SELECT * FROM discord_login_tokens WHERE access_token = $1', token);
     },
     save(token) {
-        return db.one(`INSERT INTO discord_tokens (discord_id, access_token, refresh_token, expires_in, issued_at)
-        VALUES ($/discord_id/, $/access_token/, $/refresh_token/, $/expires_in/, $/issued_at/)
+        return db.one(`INSERT INTO discord_login_tokens (discord_id, access_token, refresh_token, expires_in)
+        VALUES ($/discord_id/, $/access_token/, $/refresh_token/, $/expires_in/)
         RETURNING *`, token);
     },
     update(token) {
-        return db.one(`UPDATE discord_tokens
+        return db.one(`UPDATE discord_login_tokens
         SET
         access_token = $/access_token/,
         refresh_token = $/refresh_token/,
@@ -26,6 +26,6 @@ module.exports = {
         RETURNING *`, token);
     },
     delete(id) {
-        return db.none('DELETE FROM discord_tokens WHERE discord_id = $1', id);
+        return db.none('DELETE FROM discord_login_tokens WHERE discord_id = $1', id);
     }
 };

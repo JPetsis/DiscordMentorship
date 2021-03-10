f7e168a2d2ff11d0ac4119bb382c7afc4dcb5884
Stefan <Stefischer@mail.com>
2021-03-10 16:32:23 -0500

    Added faCogs to FontAwesome library

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faCogs, faSignOutAlt, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import HomePage from './components/pages/HomePage';
 import NavBar from './components/navigation/NavBar';
 import Footer from './components/navigation/Footer';
 import Error404 from './components/navigation/Error404';
+
+library.add(fab);
+library.add(faUserAlt, faSignOutAlt, faCogs);
 
 function App() {
 
